1df3f3558d198d93efd0d98aad388a1c953ebdac
John R Fieberg <jfieberg@umn.edu>
2021-09-08 10:50:01 -0500

    update partialr

--- R/data.R
+++ R/data.R
@@ -1,3 +1,11 @@
+#' Data sets associated with Statistics for Ecologists
+#'
+#' This package contains data sets accompanying the book,
+#' Statistics for ecologists.
+#' @docType package
+#' @name Data4Ecologists
+NULL
+
 #' @title CovidMN
 #' @description Data on covid 19 testing and results from Minnesota, downloaded on January 15, 2021.
 #' @format A data frame with 315 rows and 17 variables:
@@ -35,4 +43,67 @@
 #' @source S.L. Ross(1985). "The Effectiveness of Mental Practice in Improving the Performance of College Trombonists," Journal of Research in Music Education, Vol.33, 4, pp.221-230. \url{http://users.stat.ufl.edu/~winner/datasets.html}
 "trombone"
 
+
+#' @title RIKZdat
+#' @description Marine benthic data from nine inter-tidal areas along the Dutch coast. The data were collected by the Dutch institute RIKZ in the summer of 2002. In each inter-tidal area (denoted by ‘beach’), five samples were taken, and the macro-fauna and abiotic variables were measured.
+#' @format A data frame with 45 rows and 6 variables:
+#' \describe{
+#'   \item{\code{week}}{integer  week of sampling (1 through 4)}
+#'   \item{\code{exposure}}{integer index of exposure determined by the surf, slope, grain size, and depth of anearobic layer}
+#'   \item{\code{NAP}}{double height of sampling station compared to mean tidal level}
+#'   \item{\code{humus}}{double fraction of organic material.}
+#'   \item{\code{Beach}}{integer unique identifier for each beach that was sampled}
+#'   \item{\code{Richness}}{integer  species richness (number of species observed)}
+#'}
+#' @source Zuur, A., Ieno, E.N. and Smith, G.M., 2007. Analyzing ecological data. Springer Science & Business Media.
+"RIKZdat"
+
+
+#' @title longnosedace
+#' @description abundance data of longnose dace (*Rhinichthys cataractae*) and in-stream variables collected from the Maryland Biological Stream Survey.
+#' @format A data frame with 68 rows and 8 variables:
+#' \describe{
+#'   \item{\code{stream}}{character name of surveyed stream}
+#'   \item{\code{longnosedace}}{integer number of longnose dace (*Rhinichthys cataractae*) in each 75-meter surveyed stream section}
+#'   \item{\code{acreage}}{integer acreage drained by the stream}
+#'   \item{\code{do2}}{double dissolved oxygen (mg/liter)}
+#'   \item{\code{maxdepth}}{integer maximum depth of the 75-meter surveyed section (cm)}
+#'   \item{\code{no3}}{double nitrate concentration (mg/liter)}
+#'   \item{\code{so4}}{double sulfate concentration (mg/liter)}
+#'   \item{\code{temp}}{double water temperature on the data the stream was surveyed (degrees C).}
+#'}
+#' @source Maryland Biological Stream Survey. Downloaded from \url{http://www.biostathandbook.com/multipleregression.html}
+"longnosedace"
+
+#' @title clutch
+#' @description Data set containing Mallard (*Anas platyhynchos*) clutch sizes in two types of nest structures and cropland area within a 3.2-km radius circular buffer
+#'  of each structure in the landscape in western Minnesota during 1997–1999.
+#' @format A data frame with 139 rows and 9 variables:
+#' \describe{
+#'   \item{\code{year}}{integer year of observation}
+#'   \item{\code{strtno}}{integer unique identifier associated with each nesting structure}
+#'   \item{\code{pairs}}{double estimated number of Mallard pairs with access to the structure}
+#'   \item{\code{agr_area}}{double cropland area within a 32.2 km^2 cirucular buffur of the nest}
+#'   \item{\code{DEPLY}}{integer structure type (equal to 1 for single-cylinder structures and 2 for double-cylinder structures)}
+#'   \item{\code{CYLDR}}{integer cylinder number (1 or 2 for double-cylinder structures)}
+#'   \item{\code{CLUTCH}}{integer clutch size}
+#'   \item{\code{prcnt_area}}{double percent of the 32.2 km^2 cirucular buffur associated with crops}
+#'   \item{\code{date}}{integer Julian date (number of days since January 1 of the current year)}
+#'}
+#' @source Zicus, M. C., Fieberg, J., & Rave, D. P. (2003). Does mallard clutch size vary with landscape composition: a different view. The Wilson Journal of Ornithology, 115(4), 409-413.
+"clutch"
+
+#' @title partialr
+#' @description Simulated data set used to demonstrate added-variable and component + residual plots
+#' @format A data frame with 100 rows and 4 variables:
+#' \describe{
+#'   \item{\code{x1}}{predictor simulated using `x1 = runif(100, -5, 5)`}
+#'   \item{\code{x2}}{predictor simulated using `x2 = 0.5*x1 + rnorm(100, 0, 2)`}
+#'   \item{\code{x3}}{predictor simulated using `x3= rnorm(100, 0, 3)`}
+#'   \item{\code{x4}}{predictor simulated using `x4 = x1*0.7 + rnorm(100, 0, 4)`}
+#'   \item{\code{y}}{response variable simulated using `y =  x1*1.5 - 1.2*x2 + 0.5*x3 -0.8*x3^2 + rnorm(100, 0, 5)`}
+#'}
+#' @source Data were simulated so that y has a linear relationship with x1, a quadratic relationship with x2, and a spurious relationship
+#' with x3 (due to its correlation with x1).
+"partialr"
 
--- data-raw/mydataset.R
+++ data-raw/mydataset.R
@@ -1,4 +1,6 @@
-## code to prepare `mydataset` dataset goes here
+
+
+#sinew::makeOxygen(mydataset, add_fields = "source")
 
 #----------------------------------------------------------------
 # CovidMN data set
@@ -21,3 +23,42 @@ usethis::use_data(CovidMN, overwrite = TRUE)
 
 trombone<-read.csv("data-raw/TrombonePractice.csv")
 usethis::use_data(trombone, overwrite = TRUE)
+
+
+#---------------------------------------------------------------
+# RIKZ data
+
+RIKZdat<-read.csv("data-raw/RIKZdat.csv")
+RIKZdat <- RIKZdat %>% select(week, exposure, NAP, humus, Beach, Richness)
+usethis::use_data(RIKZdat, overwrite = TRUE)
+
+#---------------------------------------------------------------
+# Longnose Dace
+
+longnosedace <- read.csv("data-raw/longnosedace.csv")
+usethis::use_data(longnosedace, overwrite = TRUE)
+
+#---------------------------------------------------------------
+# clutch
+
+clutch <- read.csv("data-raw/Clutch.csv")
+clutch <- clutch %>% select(-nest_id)
+usethis::use_data(clutch, overwrite = TRUE)
+
+
+#---------------------------------------------------------------
+# paritalr (simulated data for partial residual plots)
+
+set.seed(1040)
+x1 <-runif(100, -5, 5)
+x2 <- 0.5*x1 + rnorm(100, 0, 2)
+x3 <- rnorm(100, 0, 3)
+x4 <- x1*0.7 + rnorm(100, 0, 4)
+y<- x1*1.5 - 1.2*x2 + 0.5*x3 -0.8*x3^2 + rnorm(100, 0, 5)
+partialr <- data.frame(x1=x1, x2=x2, x3=x3, x4=x4, y=y)
+#pairs(partialr)
+#lmpartial<-lm(y~x1+x2+x3+x4)
+#avPlots(lmpartial)
+#summary(lm(y~., data=partialr))
+#summary(lm(y~x1, data=partialr))
+usethis::use_data(partialr, overwrite = TRUE)
